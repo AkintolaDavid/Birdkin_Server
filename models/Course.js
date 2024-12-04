82f4fdb9ce018a23f008e542f9ebc84165d2ec30
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-04 16:09:37 +0100

    first

--- models/Course.js
+++ models/Course.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-  title: String,
-  rating: Number,
-  lecturer: String,
-  email: [String],
+  title: { type: String, required: true },
+  rating: { type: Number, required: true },
+  lecturer: { type: String, required: true },
+  email: [{ type: String, required: true }], // Array of emails
   description: String,
   img: String,
   category: String,
-  topics: [String],
+  topics: [{ type: String }], // Array of topics
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
