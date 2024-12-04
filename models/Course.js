59cd664526adaf1b8774cfef249f4011bf72b0ad
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-04 18:31:04 +0100

    first

--- models/Course.js
+++ models/Course.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-  id: new Date().getTime().toString(),
+  id: { type: String, unique: true, required: true }, // Use String for a UUID or timestamp
   title: { type: String, required: true },
   rating: { type: Number, required: true },
   lecturer: { type: String, required: true },
-  email: [{ type: String, required: true }], // Array of emails
+  email: [{ type: String, required: true }],
   description: String,
   img: String,
   category: String,
-  topics: [{ type: String }], // Array of topics
+  topics: [{ type: String }],
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
