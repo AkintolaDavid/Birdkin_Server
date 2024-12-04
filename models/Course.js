33b12e4ddfb03a053c11597225525c4193830462
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-04 18:36:29 +0100

    first

--- models/Course.js
+++ models/Course.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-  id: { type: String, unique: true, required: true }, // Use String for a UUID or timestamp
+  id: {
+    type: String,
+    unique: true,
+    required: true,
+    default: () => new Date().getTime().toString(), // Auto-generate a timestamp-based ID
+  },
   title: { type: String, required: true },
   rating: { type: Number, required: true },
   lecturer: { type: String, required: true },
