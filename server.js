c1710694e0db4932b6e7eb957d4965289b995acc
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-06 21:43:59 +0100

    first

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const corsOptions = {
   origin: "http://localhost:5173", // Replace with your frontend URL in production
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,POST,PUT,DELETE,PATCH",
   allowedHeaders: "Content-Type,Authorization",
 };
 
