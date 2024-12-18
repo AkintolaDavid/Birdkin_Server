e63103f1f23ae53322caeec1fd013e6eff17a452
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-18 15:20:33 +0100

    commit

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const Contact = require("../models/Contact");
 const sendOwnerMail = async (name, email, number, message) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -21,8 +22,15 @@ const sendOwnerMail = async (name, email, number, message) => {
   await transporter.sendMail(mailOptions);
 };
 router.use(bodyParser.json());
-
-router.post("/", (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: "failed to fetch contact" });
+  }
+});
+router.post("/", async (req, res) => {
   const { name, email, number, message } = req.body;
 
   if (!name || !email || !number || !message) {
@@ -30,12 +38,18 @@ router.post("/", (req, res) => {
       .status(400)
       .json({ message: "Please provide all required fields." });
   }
+  try {
+    const newContact = new Contact({ name, email, number, message });
+    await newContact.save();
 
-  sendOwnerMail(name, email, number, message);
+    await sendOwnerMail(name, email, number, message);
 
-  res.status(200).json({
-    message: "Your message has been received. Thank you!",
-  });
+    res.status(200).json({
+      message: "Your message has been received and saved. Thank you!",
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to save contact to the database." });
+  }
 });
 
 module.exports = router;
--- routes/donationRoutes.js
+++ routes/donationRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { verifyPayment } = require("../controllers/donationController");
-
+const Donation = require("../models/Donation");
 const router = express.Router();
-
+router.get("/", async (req, res) => {
+  try {
+    const donations = await Donation.find();
+    res.status(200).json(donations);
+  } catch (error) {
+    res.status(500).json({ error: "failed to fetch Donation" });
+  }
+});
 router.post("/verify-payment", verifyPayment);
 
 module.exports = router;
