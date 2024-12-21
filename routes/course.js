583ab8c0db9f80c34e059a0231a447aedc5862c0
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-21 11:56:52 +0100

    commit

--- routes/course.js
+++ routes/course.js
@@ -34,7 +34,17 @@ router.get("/:id", verifyUserToken, async (req, res) => {
     res.status(500).json({ message: "Error fetching course", error });
   }
 });
+router.delete("/:id", verifyAdminToken, async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "user deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Failed to delete user." });
+  }
+});
 const transporter = nodemailer.createTransport({
   service: "gmail", // Change if you're not using Gmail
   auth: {
--- routes/tutor.js
+++ routes/tutor.js
@@ -11,5 +11,15 @@ router.get("/", verifyAdminToken, async (req, res) => {
     res.status(500).json({ message: "Error fetching tutors", error });
   }
 });
+router.delete("/:id", verifyAdminToken, async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "user deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Failed to delete user." });
+  }
+});
 module.exports = router;
