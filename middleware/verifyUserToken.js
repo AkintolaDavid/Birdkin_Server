16d517adc5d56768072475c2b5adbfdb62da7522
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-22 09:31:09 +0100

    commit

--- middleware/verifyUserToken.js
+++ middleware/verifyUserToken.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyUserToken = (req, res, next) => {
+  console.log(req.headers.authorization);
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from Bearer
 
   if (!token) {
