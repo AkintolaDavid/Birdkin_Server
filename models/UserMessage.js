6edb23c39932731f9c41a2601b9ddf7401f54777
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-25 06:43:00 +0100

    commit

--- models/UserMessage.js
+++ models/UserMessage.js
@@ -1,21 +1,17 @@
 const mongoose = require("mongoose");
 
+const replySchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  sender: { type: String, enum: ["tutor", "user"], required: true }, // Sender type
+  date: { type: Date, default: Date.now },
+});
+
 const messageSchema = new mongoose.Schema({
   courseName: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   userMessage: { type: String, required: true },
-  date: { type: Date, required: true },
-  time: { type: String, required: true },
-  fileUrl: { type: String },
-  replies: {
-    type: [
-      {
-        content: { type: String, required: true },
-        type: { type: String, required: true, enum: ["tutor", "user"] },
-      },
-    ],
-    default: [],
-  },
+  date: { type: Date, default: Date.now },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Reference to the user
+  replies: [replySchema], // Array of replies
 });
 
 module.exports = mongoose.model("Message", messageSchema);
